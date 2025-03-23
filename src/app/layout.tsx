d31f6c366a61843d5421534dd895afcfda335a88
Toldmetodie <Toldmetodie1108@gmail.com>
2025-03-23 11:37:55 -0400

    created homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
           {children}
         </ThemeProvider>
         <Carousel />
-        {/* <Footer /> */}
+        <Footer />
       </body>
     </html>
   );
--- src/components/layouts/footer/index.tsx
+++ src/components/layouts/footer/index.tsx
@@ -3,7 +3,7 @@ import { MovieRights } from "./MovieRights";
 import { Social } from "./Social";
 export const Footer = () => {
   return (
-    <footer className=" bg-indigo-700 py-10 px-5 text-sm text-[#fafafa]">
+    <footer className=" bg-indigo-700 py-10 px-5 text-sm text-[#fafafa] fixed top-[650px] right-0 left-0">
       <div className="flex flex-col items-center justify-between gap-y-7 lg:flex-row max-w-screen-xl lg:items-start">
         <MovieRights />
         <div className="flex gap-x-12 lg: gap-x-24">

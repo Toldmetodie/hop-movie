8c99c76f870482b3170d12e6a0cdb2792c4afd24
Toldmetodie <Toldmetodie1108@gmail.com>
2025-04-23 19:05:22 -0400

    made genre appear the same color as the background

--- src/components/layouts/header/index.tsx
+++ src/components/layouts/header/index.tsx
@@ -1,23 +1,20 @@
 import { MovieZLogo } from "@/components/common";
+import { GenreDropdown } from "@/components/genre";
 import { ThemeToggleButton } from "./ThemeToggleButton";
-import { GenreDropdown } from "@/components/genre/GenreDropdown";
 import { SearchBar } from "@/components/search";
-import { Suspense } from "react";
 
 export const Header = () => {
   return (
-    <header className="fixed top-0 inset-x-0 z-20 h-[59px] flex items-center justify-center bg-white">
-      <div className="flex items-center justify-between w-full max-w-screen-xl px-5 lg:px-0 ">
+    <header className="fixed top-0 left-0 right-0 z-20 h-[59px] flex items-center justify-center bg-background">
+      <div className="flex items-center justify-between w-full max-w-screen-xl px-5 lg:px-0">
         <MovieZLogo className="text-indigo-700" />
 
-        <div className=" lg: flex items-center gap-x-3">
+        <div className="hidden lg:flex items-center gap-x-3">
           <GenreDropdown />
-          <Suspense>
-            <SearchBar isMobile={false} />
-          </Suspense>
+          <SearchBar isMobile={false} />
         </div>
 
-        <div>
+        <div className="flex items-center gap-x-3">
           <ThemeToggleButton />
         </div>
       </div>
